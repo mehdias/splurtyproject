78998d7191797cc805c36fbcb0418ee4e3563c02
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-12 21:08:06 +0000

    made changes on applicationjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require jquery_ujs
+//=require jquery
+//=require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
-//= require bootstrap
 //= require_tree .
